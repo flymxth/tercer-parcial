757e27862f7f706f537a05d980b37c32e3eb9cb6
flymxth <cugl070103hneszsa0@cbtis217.edu.mx>
2024-11-12 17:54:05 -0600

    procesos almacenados

--- PROCESOS ALMACENADOS/hoja1.sql
+++ PROCESOS ALMACENADOS/hoja1.sql
@@ -10,7 +10,26 @@ END $$
 DESCRIBE products$$
 CALL productos_categoria(6, 20) $$
 
+
+
+
 -- EJERCICIO DE HOY --
 -- ELIMINAR UN PAIS CON UN PROCESO ALMACENADO --
 
+-- 1. BORRAR LAS CIUDADES
+-- 2. BORRAR LOS LEBGUAJES
+-- 3. BORRAR EL PAIS
 
+USE WORLD;
+
+DELIMITER $$
+
+DROP PROCEDURE spBorrarPais$$
+CREATE PROCEDURE spBorrarPais(codigo CHAR(3))
+BEGIN 
+    DELETE FROM city WHERE countrycode = codigo;
+    DELETE FROM CountryLanguage WHERE countrycode = codigo;
+    DELETE FROM city WHERE code = codigo;
+END $$
+
+CALL spBorrarPais('AUT') $$
