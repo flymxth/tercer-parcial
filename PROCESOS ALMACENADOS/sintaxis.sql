66f8a0b500ad4c0de61d86506b9fd7849ca80e25
flymxth <cugl070103hneszsa0@cbtis217.edu.mx>
2024-11-13 18:16:45 -0600

    sintaxis

---
+++ PROCESOS ALMACENADOS/sintaxis.sql
@@ -0,0 +1,13 @@
+-- Cambiar el delimitador para permitir el uso de ; dentro del procedimiento
+DELIMITER $$
+
+-- Crear el procedimiento almacenado llamado 'productos_categoria'
+-- Este procedimiento recibe un parámetro de entrada 'categoria' (un número entero)
+CREATE PROCEDURE productos_categoria(IN categoria INT)
+BEGIN
+    -- Consulta los productos cuya categoría sea igual al valor de 'categoria'
+    SELECT * FROM products WHERE categoryId = categoria;
+END $$
+
+-- Restaurar el delimitador al valor por defecto (;)
+DELIMITER ;
